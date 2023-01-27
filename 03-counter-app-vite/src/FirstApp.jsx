b2103800e2251a523fd4553ff71476b8d660e2a4
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-01-27 14:07:06 -0700

    Practica del 26 de Enero en React

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,29 @@
+//import { Fragment } from "react";
+
+//------------------------------
+
+//LO QUE UTILIZO PARA JSON.STRINGIFY
+/*   const newMessage = {
+    message: 'Hola mundo',
+    tittle: 'Fernando'
+  } */
+
+//------------------------------
+
+//FUNCION QUE PUSO DE EJEMPLO EL BATO XD 
+/*   const getResult = (a, b) =>{
+    return a + b;
+  } */
+
+//-------------------------------
+
+  export const FirstApp = () => {
+  
+    return (
+      <>
+      <h1>Enrique</h1>
+      {/* <code>{ JSON.stringify (newMessage) }</code> */}
+      <p>Soy un subtitulo</p>
+      </> 
+    );
+  } 
