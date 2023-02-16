2973c4054ef1cb702c1e80afe9cd8fd346d667c9
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-02-15 23:12:23 -0700

    Sistema experto de GIF ya muestra GIF´s

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
+import { GifItem } from "./GifItem";
 import { getGifs } from "./helpers/getGifs";
 
 export const GifGrid = ({category}) => {
 
-    getGifs(category);
+  const [images, setimages] = useState([]);
+  const getImages = async() =>{
+    const newImages = await getGifs(category);
+    setimages(newImages);
+  }
 
-    return (
-    <>
-        <h3>{category}</h3>
-    </>
-  )
+  useEffect( () => {
+    getImages();
+  }, [ ])
+
+
+  return (
+  <>
+      <h3>{category}</h3>
+
+      <div className="card-grid">
+          
+          { 
+              images.map( (image) => (
+                <GifItem 
+                key={image.id} 
+                { ...image }
+                />
+              ))
+                
+          }
+        </div>
+  </>
+)
 }
--- 04-gif-expert-app/src/components/helpers/getGifs.js
+++ 04-gif-expert-app/src/components/helpers/getGifs.js
@@ -1,14 +1,14 @@
 export const getGifs = async (category) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=6pdzAAMxCNrq1OVxBPPPpi2oCZXjo20F&q=${category}&limit=20`
+   
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=6pdzAAMxCNrq1OVxBPPPpi2oCZXjo20F&q=${category}&limit=10`
     const resp = await fetch( url );
     const { data }  = await resp.json(); 
 
     const gifs = data.map(img => ({
         id: img.id,
-        tittle: img.tittle,
+        title: img.title,
         url: img.images.downsized_medium.url
     }))
-    
-    console.log(gifs);
+
     return gifs;
 }
