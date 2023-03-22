2bf8640615283e01cf3967c0d2c868e523ab6bd9
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-03-21 23:53:22 -0700

    useReducer y TODOS en React

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
-
-
+//import { Padre } from './07-tarea-memo/Padre'
+////////////////////////////////////////////////////////////////
+//import { CallbackHook } from './06-memos/CallbackHook'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { Memorize } from './06-memos/Memorize'
 //import { MultipleCustomHook } from './03-example'
@@ -16,9 +15,11 @@ import { Padre } from './07-tarea-memo/Padre'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { HooksApp } from './HooksApp' */
 import './index.css'
+//import './08-useReducer/intro-reducer'
+import { TodoApp } from './08-useReducer/TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-//  <React.StrictMode>
-    <Padre />
-//  </React.StrictMode>
+//<React.StrictMode>
+    <TodoApp />
+//</React.StrictMode>
 )
