4be7b3d9d9e0a4fd3d130c11af04f1b727b2287f
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-04-04 17:30:06 -0700

    Se agregan todos los archivos de 07-heroes-spa Parte 1

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes, Link } from "react-router-dom"
 import { AboutPage } from "./AboutPage"
+import { UserProvider } from "./context/UserProvider"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
 import { Navbar } from "./Navbar"
 
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
         {/* <h1>MainApp</h1> */}
         {/* <Link to="/">Home</Link>
         <Link to="/about">About</Link>
@@ -21,9 +22,10 @@ export const MainApp = () => {
              <Route path="/" element={<HomePage />}/>
              <Route path="about" element={<AboutPage />}/>
              <Route path="login" element={<LoginPage />}/>
+              
            {/*   <Route path="/*" element={<LoginPage />}/> */}
              <Route path="/*" element={<Navigate to="/about" />}/>
         </Routes>
-    </>
+    </UserProvider>
   )
 }
